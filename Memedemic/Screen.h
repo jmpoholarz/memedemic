064da6a76bb7122f4bb8fc1561bb581b3dd75abe
amring <amring777@gmail.com>
2017-09-16 18:17:09 -0400

    Added functions to Screen.h

--- Memedemic/Screen.h
+++ Memedemic/Screen.h
@@ -1,11 +1,14 @@
 #pragma once
-
+#include <string>
+#include "Board.h"
 class Screen {
 
 public:
 	Screen();
 	~Screen();
-
+	Board getBoard();
+	void showBoard();
+	void sendCommand(std::string command);
 private:
 
 };

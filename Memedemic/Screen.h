33030824e5b991e99212e468b94d4a03a414fbb8
amring <amring777@gmail.com>
2017-09-16 18:18:19 -0400

    removed getBoard function

--- Memedemic/Screen.h
+++ Memedemic/Screen.h
@@ -1,12 +1,10 @@
 #pragma once
 #include <string>
-#include "Board.h"
 class Screen {
 
 public:
 	Screen();
 	~Screen();
-	Board getBoard();
 	void showBoard();
 	void sendCommand(std::string command);
 private:

c8c6d9276a567169cbca903a5f1ac7b27e3e1d48
jmpoholarz <jmpoholarz@gmail.com>
2017-09-19 21:23:35 -0400

    More Parser code; need to change function returns from bool to int in GameStateManager to let the Parser work better

--- Memedemic/Parser.cpp
+++ Memedemic/Parser.cpp
@@ -30,70 +30,178 @@ std::string Parser::parse(std::string command) {
 		return getUsage(tokens[1]);
 	}
 	else if (tokens[0] == "access") {
-		// Wrong number of arguments
+		// Check for wrong number of arguments
 		if (tokens.size() != 2)
 			return "Incorrect usage of access: " + getUsage("access");
 		// Get location number from enum
 		int location = convertCard(tokens[1]);
 		// Check if valid location
 		if (!(location >= 0 && location <= 23)) {
-			return "Invalid location: " + getUsage("access");
+return "Invalid location: " + getUsage("access");
 		}
-		
+
 		// Attempt to move player
 		bool successful = gsm.movePlayer(location);
-		if (successful) 
+		if (successful)
 			return "Moved to " + tokens[1];
 		else return "Unable to move to " + tokens[1];
-
 	}
 	else if (tokens[0] == "build") {
-
+		// Check for wrong number of arguments
+		if (tokens.size() != 1) {
+			return "Incorrect usage of build: " + getUsage("build");
+		}
+		// Attempt to build
+		bool successful = gsm.buildCMCServer();
+		if (successful)
+			return "Built CMC Server.";
+		else return "Unable to build CMC Server.";
 	}
 	else if (tokens[0] == "ban") {
-
+		// Check for wrong number of arguments
+		if (tokens.size() != 2) {
+			return "Incorrect usage of ban: " + getUsage("ban");
+		}
+		// Attempt to ban disease
+		int whichMeme = atoi(tokens[1].c_str());
+		bool successful = gsm.banMeme(whichMeme);
+		if (successful)
+			return "Banned meme " + tokens[1] + " from current location.";
+		else return "Unable to ban meme " + tokens[1] + " from current location.";
 	}
 	else if (tokens[0] == "give") {
-
+		// Check for wrong number of arguments
+		if (tokens.size() != 3) {
+			return "Incorrect usage of give: " + getUsage("give");
+		}
+		// Attempt to give card
+		int card = convertCard(tokens[1]);
+		/// TODO
 	}
 	else if (tokens[0] == "take") {
-
+		// Check for wrong number of arguments
+		if (tokens.size() != 3) {
+			return "Incorrect usage of give: " + getUsage("take");
+		}
+		// Attempt to take card
+		int card = convertCard(tokens[1]);
+		/// TODO
 	}
 	else if (tokens[0] == "filter") {
+		// Check for wrong number of arguments
+		if (tokens.size() != 6 && tokens.size() != 5) {
+			return "Incorrect usage of give: " + getUsage("filter");
+		}
 
+		/// TODO
 	}
 	else if (tokens[0] == "event") {
+		// Check for wrong number of arguments
+		if (tokens.size() != 2 && tokens.size() != 3) {
+			return "Incorrect usage of event: " + getUsage("event");
+		}
+		// Attempt to play card
+		int card = convertCard(tokens[1]);
 
+		/// TODO
 	}
 	else if (tokens[0] == "outbreak") {
-
+		// Check for wrong number of arguments
+		if (tokens.size() != 2 && tokens.size() != 3) {
+			return "Incorrect usage of outbreak: " + getUsage("outbreak");
+		}
+		// Try to print outbreak track
+		/// TODO
 	}
 	else if (tokens[0] == "viral") {
-
+		// Check for wrong number of arguments
+		if (tokens.size() != 2 && tokens.size() != 3) {
+			return "Incorrect usage of viral: " + getUsage("viral");
+		}
+		// Try to print viral quotient
+		/// TODO
 	}
 	else if (tokens[0] == "meme") {
-
+		// Check for wrong number of arguments
+		if (tokens.size() != 2 && tokens.size() != 3) {
+			return "Incorrect usage of meme: " + getUsage("meme");
+		}
+		// Attempt to print meme status
+		/// TODO
 	}
 	else if (tokens[0] == "players") {
+		// Check for wrong number of arguments
+		if (tokens.size() != 2 && tokens.size() != 3) {
+			return "Incorrect usage of players: " + getUsage("players");
+		}
 
+		/// TODO
+	}
+	else if (tokens[0] == "roles") {
+		// Check for wrong number of arguments
+		if (tokens.size() != 2 && tokens.size() != 3) {
+			return "Incorrect usage of roles: " + getUsage("roles");
+		}
+
+		/// TODO
 	}
 	else if (tokens[0] == "cmc") {
+		// Check for wrong number of arguments
+		if (tokens.size() != 2 && tokens.size() != 3) {
+			return "Incorrect usage of cmc: " + getUsage("cmc");
+		}
 
+		/// TODO
+	}
+	else if (tokens[0] == "servers") {
+		// Check for wrong number of arguments
+		if (tokens.size() != 2 && tokens.size() != 3) {
+			return "Incorrect usage of servers: " + getUsage("servers");
+		}
+
+		/// TODO
 	}
 	else if (tokens[0] == "draw") {
+		// Check for wrong number of arguments
+		if (tokens.size() != 2 && tokens.size() != 3) {
+			return "Incorrect usage of draw: " + getUsage("draw");
+		}
 
+		/// TODO
 	}
 	else if (tokens[0] == "discard") {
+		// Check for wrong number of arguments
+		if (tokens.size() != 2 && tokens.size() != 3) {
+			return "Incorrect usage of discard: " + getUsage("discard");
+		}
 
+		/// TODO
 	}
 	else if (tokens[0] == "cards") {
+		// Check for wrong number of arguments
+		if (tokens.size() != 2) {
+			return "Incorrect usage of cards: " + getUsage("cards");
+		}
 
+		/// TODO
 	}
 	else if (tokens[0] == "end") {
-
+		// Check for wrong number of arguments
+		if (tokens.size() != 1 && tokens.size() != 2) {
+			return "Incorrect usage of end: " + getUsage("end");
+		}
+		// Attempt to end turn
+		bool successful = gsm.nextTurn();
+		if (successful) return "Proceeding to next turn.";
+		else return "Unable to end turn.";
 	}
 	else if (tokens[0] == "new") {
-
+		// Check for wrong number of arguments
+		if (tokens.size() != 1) {
+			return "Incorrect usage of new: " + getUsage("new");
+		}
+		// Attempt to start new game
+		/// TODO
 	}
 }
 
@@ -232,6 +340,9 @@ std::string Parser::getUsage(std::string command) {
 		}
 		else if (command == "cmc") {
 			return "cmc servers";
+		}
+		else if (command == "servers") {
+			return "servers";
 		}
 		else if (command == "draw") {
 			return "draw cards";

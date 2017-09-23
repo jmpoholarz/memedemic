b4316ebae256e8f6902fab7970df0fd87cf56ed5
trankq <trankq@purdue.edu>
2017-09-22 20:29:07 -0400

    finished testcases

--- Memedemic/LocationTest.cpp
+++ Memedemic/LocationTest.cpp
@@ -2,7 +2,7 @@
 #include <string.h>
 #include "Location.h"
 
-void locationTest(Location& location, int l,std::vector<int> list, const char *expectation) {
+void locationTest(Location& location, int l,std::vector<int> list, const char *expectation, int number) {
     std::vector<int> r = location.getAdjacentLocations(l);
     bool passing;
     if (list == r) {
@@ -11,33 +11,54 @@ void locationTest(Location& location, int l,std::vector<int> list, const char *e
         passing = false;
     }
     if (strcmp(expectation, "pass") == 0 && passing) {
-        std::cout << "passed" << std::endl;
+        std::cout << "Test " << number << " passed" << std::endl;
         return;
     } else {
-        std::cout << "failed" << std::endl;
+        std::cout << "Test " << number << " failed" << std::endl;
         return;
     }
     if (strcmp(expectation,"fail") == 0 && !passing) {
-        std::cout << "passed" << std::endl;
+        std::cout << "Test " << number << " passed" << std::endl;
         return;
     } else {
-        std::cout << "failed" << std::endl;
+        std::cout << "Test " << number << " failed" << std::endl;
         return;
     }
 }
 
+void adjacentTest(Location& location, int one, int two, const char *expectation, int number) {
+    if (strcmp(expectation,"pass") == 0 && location.isAdjacent(one, two) == 1) {
+        std::cout << "Test " << number << " passed" << std::endl;
+        return;
+    } else {
+        std::cout << "Test " << number << " failed" << std::endl;
+        return;
+    }
+    if (strcmp(expectation,"fail") == 0 && location.isAdjacent(one, two) == 0) {
+        std::cout << "Test " << number << " passed" << std::endl;
+        return;
+    } else {
+        std::cout << "Test " << number << " failed" << std::endl;
+        return;
+    }
+    
+}
+
 int main() {
 	
-	// Create GameStateManager to setup game
 	Location* location = new Location();
-    std::cout << "test" << std::endl;
+    std::cout << "Tests" << std::endl;
     
     std::vector<int> list = {1, 2, 15};
-    locationTest(*location,0,list, "pass");
+    locationTest(*location,0,list, "pass", 1);
     std::vector<int> list2 = {11,12};
-    locationTest(*location,10,list2, "pass");
+    locationTest(*location,10,list2, "pass", 2);
     std::vector<int> list3 = {22,19};
-    locationTest(*location,23,list3, "pass");
-    locationTest(*location,19,list3, "fail");
+    locationTest(*location,23,list3, "pass", 3);
+    locationTest(*location,19,list3, "fail", 4);
+
+    adjacentTest(*location, 19, 14, "pass", 5);
+    adjacentTest(*location, 9, 8, "pass", 6);
+    adjacentTest(*location, 5, 9, "fail", 7);
     
 }

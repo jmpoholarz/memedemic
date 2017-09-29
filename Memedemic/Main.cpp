608e0e7fac4a665aa7ba79c54570fd608b592b3a
jmpoholarz <jmpoholarz@gmail.com>
2017-09-29 17:15:09 -0400

    Fixed non-integer input crashing setup

--- Memedemic/Main.cpp
+++ Memedemic/Main.cpp
@@ -4,19 +4,15 @@
 int main() {
 	// Prompt player count
 	int players = 0;
+	std::string strPlayers = "";
 	do {
-		std::cout << "Enter players 1-4:" << std::endl;
-		try
-		{
-			std::cin >> players;
-		}
-		catch (const std::exception&)
-		{
-			continue;
-		}
+		std::cout << "Enter players (1-4): ";
+		std::getline(std::cin, strPlayers);
+		if (strPlayers.size() == 1 && std::isdigit(strPlayers[0]))
+			players = atoi(strPlayers.c_str());
 	} while (!(players > 0 && players < 5));
-	std::string s = ""; // throwaway string to fix extra empty line error
-	std::getline(std::cin,s); // eat empty line
+	//std::string s = ""; // throwaway string to fix extra empty line error
+	//std::getline(std::cin,s); // eat empty line
 	// Create GameStateManager to setup game
 	GameStateManager* gsm = new GameStateManager(players);
 	Parser* parser = new Parser(*gsm);
